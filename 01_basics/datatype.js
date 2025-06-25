c80fad8867bfb97c242b02a41a7bc731fdb419b3
Abhishek Solanki <93044135+Abhishek31702@users.noreply.github.com>
2025-06-25 18:49:59 +0000

    add datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all the code as newest js
+
+// alert(4+3) we are using nodejs, not browser
+
+console.log(3+3)  //code readablity should be high
+
+console.log("hello_world")
+
+let name = "abhishek"
+let age = 22
+let isLoggedin = false
+let state;
+
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof 23);
+
+console.log(typeof age);
+console.log(typeof null);
