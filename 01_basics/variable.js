76d13547fbbd03e8000f6d898d7035c776438132
Abhishek Solanki <93044135+Abhishek31702@users.noreply.github.com>
2025-06-25 13:32:42 +0000

    changes done

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 134987
+let accountEmail = "abhishek@gmail.com"
+var accountPassword = "12345"
+accountCity = "mumbai" 
+let accountState
+
+// accountId = 34 // not allowed
+accountEmail = "avi@gmail.com"
+accountPassword = "3467787"
+accountCity = "pune"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/* not to use var */
