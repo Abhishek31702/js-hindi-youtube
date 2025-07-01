5af3c2f90fbc03d87bc86d5159fb7ab25652e82e
Abhishek Solanki <93044135+Abhishek31702@users.noreply.github.com>
2025-07-01 12:49:59 +0000

    add date and time

---
+++ 01_basics/dates.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleString())
+
+let myCreateddate = new Date(2023,6,31)
+console.log(myCreateddate.toDateString())
