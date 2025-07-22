8246f04bb6800b0be8d48f342bad2f5610ba0a22
Abhishek Solanki <93044135+Abhishek31702@users.noreply.github.com>
2025-07-22 04:44:41 +0000

    latest

---
+++ 03_basics/arrow.js
@@ -0,0 +1,19 @@
+const user = {
+    username : "hitesh",
+    price : 888,
+    welcomeUser : function() {
+        console.log(this)
+    }
+}
+// user.welcomeUser()
+// user.username = 'ram'
+// user.welcomeUser()
+
+// console.log(this);
+
+// function chai (){
+//     console.log(this);
+    
+// }
+
+// chai()
--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -5,9 +5,49 @@ if (true){
     let a = 10
     const b = 20 
     // var c = 30
-    console.log(`inter:${a}`)
+    // console.log(`inter:${a}`)
 }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+// function one (){
+//     const username = "avishek"
+
+//     function two (){
+//         const website = "youtube"
+//         console.log(username)
+//     }
+//     // console.log(website)
+
+//     two()
+// }
+
+// one()
+
+
+if (true) {
+    const username = "avishek"
+
+    if (username=== "avishek") {
+        const channel = "youtube"
+        // console.log(username + channel)
+    }
+
+    // console.log(channel)
+}
+
+// console.log(username)
+
+
+console.log(addOne(4))
+
+function addOne(num){
+    return num + 1
+}
+// console.log(addtwo)
+const addtwo = function(num){
+    return num +1
+}
