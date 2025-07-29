3ace7cf58413c3e2ac7b31abe0615d74edc9af81
Abhishek Solanki <93044135+Abhishek31702@users.noreply.github.com>
2025-07-29 18:59:45 +0000

    Pending changes exported from your codespace

---
+++ 05_iteration/four.js
@@ -0,0 +1,12 @@
+// const coding = ["cpp","java","python","js","ruby"]
+
+// const value = coding.forEach( (item) =>{
+//     // 
+//     // console.log(item);
+//     return item;
+    
+// })
+
+// console.log(value)
+
+const myNum = [1,2,3,4,5,6,7]
--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -1,32 +1,33 @@
 // for of
 
 
-const arr = [1,2,3,4,5]
+// const arr = [1,2,3,4,5]
 
-for (const num of arr) {
-    // console.log(num);
+// for (const num of arr) {
+//     // console.log(num);
     
-}
+// }
 
 
-const string = "helloworld"
+// const string = "helloworld"
 
-for (const element of string) {
-    // console.log(element);
+// for (const element of string) {
+//     // console.log(element);
     
-}
+// }
 
-const greeting = "hello world"
-for (const greet of greeting) {
-    console.log(greet)
-}
+// const greeting = "hello world"
+// for (const greet of greeting) {
+//     console.log(greet)
+// }
 
 
-// const map = new Map()
-// map.set('IND','INDIA')
+const map = new Map()
+map.set('IND','INDIA')
+map.set('FR','FRANCE')
 
 // console.log(map);
 
-// for (const key of map) {
-//     console.log(key)
-// }
+for (const [key,value] of map) {
+    console.log(key,':-',value)
+}
