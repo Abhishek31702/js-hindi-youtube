d257bb07c9b161b88f40164428778bfe87849f32
Abhishek Solanki <93044135+Abhishek31702@users.noreply.github.com>
2025-08-08 06:28:56 +0000

    add ps checker proj

--- project3/script.js
+++ project3/script.js
@@ -2,29 +2,47 @@ let password = document.querySelector("#password");
 let strength = document.querySelector("#strength");
 let button = document.querySelector("#button");
 let number = document.querySelector("#number");
+let uppercase = document.querySelector("#uppercase");
+let char = document.querySelector("#char");
 
 button.addEventListener("click", function () {
     let passValue = password.value;
     strength.textContent = "";
     number.textContent = "";
+    uppercase.textContent = "";
+    char.textContent = "";
 
-    // First check: Length
+    
     if (passValue.length >= 8) {
         strength.append("Password fulfills the first criteria of minimum 8 letters");
 
-        // Second check: Numbers (only runs if length is valid)
+        
         let hasNumbers = /[0-9]/.test(passValue);
 
         if (hasNumbers) {
             number.append("Password fullfills the second criteria of 0-9");
+
+            let hasUpperCase = /[A-Z]/.test(passValue);
+            if (hasUpperCase) {
+                uppercase.append("Password fullfills the third criteria of uppercase");
+                let hasChar = /[!@#$%^&*]/.test(passValue);
+
+                if(hasChar) {
+                    char.append("Password fullfills the criteria of character")
+                } else {
+                    char.append("Password doesnt contain an character")
+                }
+            } else {
+                uppercase.append("Password does not contain an uppercase letter");
+}
         } else {
             number.append("Password doesn't include numbers");
         }
 
-        // Future checks (like uppercase, symbols) will go here...
+        
 
     } else {
         strength.append("Password doesn't have 8 letters");
-        // Do not run other checks
+        
     }
 });
